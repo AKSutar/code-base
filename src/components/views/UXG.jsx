3c676187b6e155161500658d037fc4b9b7bae76d
AKSutar <sonuthesilentkiller@gmail.com>
2021-07-14 09:25:16 +0530

    second commit

---
+++ src/components/views/UXG.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+import { Main} from "../molecules";
+import { Header, Footer, Offshore } from "../organisms";
+
+import { connect } from "react-redux";
+
+const connectedProps = (state) => ({
+  settingsOpen: state.common.settingsOpen,
+});
+
+const connectionActions = {
+}
+
+var connector = connect(connectedProps, connectionActions);
+
+const PageWrapper = styled.div``;
+
+export const UXG = (props) => {
+  return <PageWrapper>
+      { !props.preview && <> 
+        <Header />
+        <Offshore />
+      </>}
+      <Main/>
+      <Footer/>
+    </PageWrapper>;
+};
+
+UXG.defaultProps = {
+  selected: false,
+};
+
+export default connector(UXG);
